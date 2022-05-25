dceeef12e1af92dbeb5e6f2f8911f47c66a93dae
v.dddemeneva <v.dddemeneva@gmail.com>
2022-05-25 12:07:23 +0300

    feat: refactor

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -2,7 +2,6 @@ const path = require("path");
 const fs = require("fs");
 const BASE = "utf-8";
 const BANDLE_PATH = path.join(__dirname, "project-dist", "bundle.css");
-const READ_PATH = path.join(__dirname, ".\\styles", file.name);
 //Create a path
 fs.writeFile(BANDLE_PATH, "", (err) => {
   if (err) throw err;
@@ -18,7 +17,7 @@ fs.readdir(
     } else {
       files.forEach((file) => {
         if (path.extname(file.name) === ".css" && file.isFile() === true) {
-          fs.readFile(READ_PATH, BASE, (err, data) => {
+          fs.readFile(path.join(__dirname, ".\\styles", file.name), BASE, (err, data) => {
             if (err) throw err;
             fs.appendFile(BANDLE_PATH, data, (err) => {
               if (err) throw err;
